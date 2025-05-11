864b2c22ee50bab41f3f0194a859944547bc5325
selkirks <selkirks@users.noreply.github.com>
2025-05-11 16:49:27 +0000

    Fix file encoding

--- scripts/screens/SettingsScreen.py
+++ scripts/screens/SettingsScreen.py
@@ -744,7 +744,7 @@ class SettingsScreen(Screens):
             )
             self.checkboxes["es"] = UISurfaceImageButton(
                 ui_scale(pygame.Rect((310, 0), (180, 37))),
-                "español",
+                "espanol",
                 get_button_dict(ButtonStyles.LADDER_MIDDLE, (180, 37)),
                 object_id="@buttonstyles_ladder_middle",
                 manager=MANAGER,
